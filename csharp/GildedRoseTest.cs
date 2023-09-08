b7d2eec49dadeee90fd398dbfc006eac21ee6bf6
Stefan <simanek.s@gmx.at>
2023-09-08 12:51:01 +0200

    cleanup

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -7,7 +7,7 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void Foo()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
