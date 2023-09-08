27b9aeaa670ac115d668901ef840b2b0983d7287
Stefan <simanek.s@gmx.at>
2023-09-08 20:42:01 +0200

    fixes

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -5,7 +5,7 @@ namespace csharp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine("OMGHAI!");
 
