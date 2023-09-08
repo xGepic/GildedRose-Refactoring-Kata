b6397f68989fc0721e3fb11be7f564ccf3935695
Stefan <simanek.s@gmx.at>
2023-09-08 13:47:23 +0200

    renaming

--- csharp/Entities/Item.cs
+++ csharp/Entities/Item.cs
@@ -5,7 +5,6 @@
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
-
         public override string ToString()
         {
             return this.Name + ", " + this.SellIn + ", " + this.Quality;
--- csharp/Tests/GildedRoseTest.cs
+++ csharp/Tests/GildedRoseTest.cs
@@ -8,24 +8,24 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void EndOfDaySellInAndQualityDecrease()
+        public void AgedBrieSellInAndQualityDecrease()
         {
             //Arrange
-            GildedRose app = new GildedRose(new List<Item> { new Item { Name = "Aged Item", SellIn = 2, Quality = 2 } });
+            GildedRose app = new GildedRose(new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 2 } });
             //Act 
             app.UpdateQuality();
             //Assert
-            TestUtil.CustomItemAssertEquals(new Item { Name = "Aged Item", SellIn = 1, Quality = 1 }, app.GetItems()[0]);
+            TestUtil.CustomItemAssertEquals(new Item { Name = "Aged Brie", SellIn = 1, Quality = 3 }, app.GetItems()[0]);
         }
         [Test]
-        public void SellByDatePassedAndQualityDecreasesTwice()
+        public void AgedBrieSellByDatePassedAndQualityDecreasesTwice()
         {
             //Arrange
-            GildedRose app = new GildedRose(new List<Item> { new Item { Name = "Aged Item", SellIn = 0, Quality = 2 } });
+            GildedRose app = new GildedRose(new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 4 } });
             //Act
             app.UpdateQuality();
             //Assert
-            TestUtil.CustomItemAssertEquals(new Item { Name = "Aged Item", SellIn = -1, Quality = 0 }, app.GetItems()[0]);
+            TestUtil.CustomItemAssertEquals(new Item { Name = "Aged Brie", SellIn = -1, Quality = 0 }, app.GetItems()[0]);
         }
     }
 }
